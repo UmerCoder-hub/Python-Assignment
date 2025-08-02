5468fbe76a96b15c28a1c4c7a02e35ad5b595eb7
UmerCoder-hub <mu225239@gmail.com>
2025-08-02 20:23:22 +0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# Take username and password input from user
-username = input("Enter your username: ")
-password = input("Enter your password: ")
-
-# Compare with predefined username and password
-if username == "Talha" and password == "Axiom123":
-    print("Login successful!")
-else:
-    print("Invalid username or password.")
