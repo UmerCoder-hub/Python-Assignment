99d29f8e4f7b2eba8219e73a252af7fb34b7cfea
UmerCoder-hub <mu225239@gmail.com>
2025-08-15 21:38:20 +0500

    Update Q1.py

--- Assignment_3/Part_2/Q1.py
+++ Assignment_3/Part_2/Q1.py
@@ -1,12 +1,4 @@
 #Create a list to store student data i.e. name, age, course and is_attending. Display each element of list using for loop.
-name=input("Enter Your name: ")
-age=int(input("Enter Your age: "))
-course=input("Enter your Course: ")
-is_attending=True
-student=[]
-student.append(name)
-student.append(age)
-student.append(course)
-student.append(is_attending)
-for item in student:
+List = [“Tahir”, 44, “AI and Data Science”, True]
+for item in List:
     print(item)
