a670ca314e472e14860c1c0b7a150d7e293887ef
UmerCoder-hub <mu225239@gmail.com>
2025-08-15 20:53:16 +0500

    Create Q1.py

---
+++ Assignment_3/Part_1/Q1.py
@@ -0,0 +1,5 @@
+#Write a function named “Greetings” that takes user’s name and print greeting.
+#Output:       Welcome to SMIT training center, Ahsan 
+
+def Greeting(name):
+  return f"Welcome to SMIT training center, {name}"
